c13792eb2bc4eaeebce728db4368508929109b02
Oleksandr <148046871+HXLFD@users.noreply.github.com>
2024-12-07 12:08:16 +0200

    Update AppDbContext.cs

--- ConferenceRoomsApi/ConferenceDbContext/AppDbContext.cs
+++ ConferenceRoomsApi/ConferenceDbContext/AppDbContext.cs
@@ -19,52 +19,20 @@ namespace ConferenceRoomsApi.ConferenceDbContext
         {
             modelBuilder.HasDefaultSchema("Rooms");
 
-            // Настройка таблицы Room
             modelBuilder.Entity<Room>(entity =>
             {
                 entity.ToTable("Room");
             });
 
-            // Настройка таблицы Service
             modelBuilder.Entity<Service>()
            .HasMany(s => s.RoomServices)
            .WithOne(rs => rs.Service)
            .HasForeignKey(rs => rs.ServiceId);
 
 
-            // Настройка таблицы-связки RoomService
             modelBuilder.Entity<RoomService>()
            .HasKey(rs => new { rs.RoomId, rs.ServiceId });
-            //    modelBuilder.Entity<RoomService>(entity =>
-            //    {
-            //        entity.ToTable("RoomServices");
-            //        entity.HasKey(rs => new { rs.RoomId, rs.ServiceId });
-
-            //        entity.HasOne(rs => rs.Room)
-            //              .WithMany(r => r.RoomServices)
-            //              .HasForeignKey(rs => rs.RoomId)
-            //              .OnDelete(DeleteBehavior.Cascade);
-
-            //        entity.HasOne(rs => rs.Service)
-            //              .WithMany(s => s.RoomServices)
-            //              .HasForeignKey(rs => rs.ServiceId)
-            //              .OnDelete(DeleteBehavior.Cascade);
-            //    });
-
-            //    modelBuilder.Entity<RoomService>()
-            //.HasKey(rs => new { rs.RoomId, rs.ServiceId });
-
-            //    modelBuilder.Entity<RoomService>()
-            //        .HasOne(rs => rs.Room)
-            //        .WithMany(r => r.RoomServices)
-            //        .HasForeignKey(rs => rs.RoomId)
-            //        .OnDelete(DeleteBehavior.Cascade); // Каскадное удаление
-
-            //    modelBuilder.Entity<RoomService>()
-            //        .HasOne(rs => rs.Service)
-            //        .WithMany(s => s.RoomServices)
-            //        .HasForeignKey(rs => rs.ServiceId);
+            
         }
     }
 }
-// --project C:\Users\ARW10\source\repos\ConferenceRoomsApi\ConferenceRoomsApi\ConferenceRoomsApi.csproj
