63df4e7a27107bba52e88a4c017a6fbd7d6541c2
Oleksandr <148046871+HXLFD@users.noreply.github.com>
2024-12-09 10:37:09 +0200

    Add files via upload

--- ConferenceRoomsApi/Migrations/20241209082258_InitialCreate.Designer.cs
+++ ConferenceRoomsApi/Migrations/20241209082258_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using ConferenceRoomsApi.ConferenceDbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ConferenceRoomsApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241209082258_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
